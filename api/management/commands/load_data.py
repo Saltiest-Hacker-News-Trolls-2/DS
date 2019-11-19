579f20a7f1b34489156b9396808c8dafa190aa64
Qwelian Tanner <qweliant@gmail.com>
2019-11-19 14:36:41 -0500

    loading data works: more integrations

--- api/management/commands/load_data.py
+++ api/management/commands/load_data.py
@@ -8,9 +8,6 @@ import html2text
 
 
 class Command(BaseCommand):
-
-
-
     help = 'Overwrites titles in database'
 
     # def add_arguments(self, parser):
@@ -20,34 +17,39 @@ class Command(BaseCommand):
         Items.objects.all().delete()
     
         print("loading data from Hacker News api... ")
-        url = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(int(requests.get('https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty').json()))
-        response = requests.get(url)
 
-        if response.status_code == 200:
-            print(response.text)
-            if response.json()['type'] == 'comment':
-                print('ok')
-                # removes html characters from data
-                text_maker = html2text.HTML2Text()
-                text_maker.ignore_links = True
-                text_maker.bypass_tables = False
-                text_maker.open_quote = True
-                text_maker.close_quote = True
-                text_conv = text_maker.handle(response.json()['text'])
+        url = 'https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty'
+        req = requests.get(url).json()
 
-                Items.objects.create(
-                    id = response.json()['id'],
-                    # deleted = response.json()['deleted'],
-                    type = response.json()['type'],
-                    by = response.json()['by'],
-                    time = response.json()['time'],
-                    # dead = response.json()['dead'],
-                    # kids = response.json()['kids'],
-                    parent = response.json()['parent'],
-                    text = text_conv,
-                )
+        # removes html characters from data
+        text_maker = html2text.HTML2Text()
+        text_maker.ignore_links = True
+        text_maker.bypass_tables = False
+        text_maker.open_quote = True
+        text_maker.close_quote = True
+
+        for i in range(1, 41):
+            response = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{i}.json?print=pretty')
+            if response.status_code == 200:            
+                if response.json()['type'] == 'comment':
+                    print(response.text)
+                    print('ok')
+
+                    text_conv = text_maker.handle(response.json()['text'])
+
+                    Items.objects.create(
+                        id = response.json()['id'],
+                        # deleted = response.json()['deleted'],
+                        type = response.json()['type'],
+                        by = response.json()['by'],
+                        time = response.json()['time'],
+                        # dead = response.json()['dead'],
+                        # kids = response.json()['kids'],
+                        parent = response.json()['parent'],
+                        text = text_conv,
+                    )
             else:
-                print('nope')
+                print('nope', response.status_code)
 
         
         
--- api/models.py
+++ api/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone 
 from django.core.validators import int_list_validator
 
 # Create your models here.
@@ -8,9 +7,9 @@ class Items(models.Model):
     deleted = models.BooleanField(default=False)
     type = models.CharField(max_length=15)
     by = models.CharField(max_length=50)
-    time = models.IntegerField()
+    time = models.IntegerField(default=0)
     dead = models.BooleanField(default=False)
-    kids = models.IntegerField(default=0, validators=[int_list_validator])
+    kids = models.CharField(max_length=300, validators=[int_list_validator])
     parent = models.IntegerField(default=0)
     text = models.TextField(default='')
 
@@ -20,5 +19,6 @@ class Items(models.Model):
 
 class SaltyUser(models.Model):
     by = models.ForeignKey(Items, on_delete=models.CASCADE)
-    score = models.DecimalField(max_digits=8, decimal_places=3)
+    salty_score = models.DecimalField(max_digits=8, decimal_places=3)
+    sarcasm_core = models.DecimalField(max_digits=8, decimal_places=3)
     catagory = models.CharField(default='', max_length=15)
